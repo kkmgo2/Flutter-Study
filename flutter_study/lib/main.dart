016a2b4dfb9aafa9f307f79f7a5eaeaaba197df6
Kyungmo Kim <kyungmo.kim@autodesk.com>
2020-10-13 21:13:24 +1100

    Created a flutter project with simple app

---
+++ flutter_study/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+/*void main(){
+  runApp(MyApp());
+}*/
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print('Answer chosen!');
+  }
+
+  @override
+  Widget build(BuildContext context){
+    var questions = [
+      'What\s your favorite color?',
+      'What\s your favorite animals?'
+    ];
+
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text('My First App')),
+      body: Column(children: [
+        Text('The question!'),
+        RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
+        RaisedButton(child:Text('Answer 2'), onPressed: answerQuestion),
+        RaisedButton(child:Text('Answer 3'), onPressed: answerQuestion,)
+      ],),
+    ),);
+  }
+}
